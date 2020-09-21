458fde9f9d6c7acd83660a56dabd555bd84f4bf4
liangshan <liang.shan@zhaopin.com.cn>
2020-09-21 14:44:46 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+html,
+body {
+  outline: none;
+  height: 100%;
+}
+#app {
+  height: 100%;
+}
+.hljs-name {
+  color: #22863a;
+}
+
+.hljs-attr {
+  color: #6f42c1;
+}
+
+.hljs-keyword {
+  color: red;
+}
+
+.hljs-string {
+  color: #032f62;
+}
+</style>
